0223f206536b69fa23329cad9169e51408312556
Andrew Lee <alee68@fordham.edu>
2022-07-20 00:48:33 -0400

    finished homepage routes

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const { Where } = require("sequelize/types/utils");
 const { User, Post, Comment } = require("../../models");
+const withAuth = require("../utils/auth");
 
 // get all of the posts
 router.get("/", async (req, res) => {
@@ -22,7 +24,7 @@ router.get("/", async (req, res) => {
 // get single post, give me POST, comment, and comment user
 router.get("/posts/:id", async (req, res) => {
 	try {
-		const postData = await Post.findByPK({
+		const postData = await Post.findByPk({
 			include: [
 				{
 					model: User,
@@ -47,10 +49,49 @@ router.get("/posts/:id", async (req, res) => {
 });
 
 // dashboard: get all posts of user through user id
+router.get("/dashboard", withAuth, async (req, res) => {
+	try {
+		const postData = await Post.findAll({
+			where: {
+				user_id: req.session.user_id,
+			},
+		});
+
+		const dashPosts = postData.map((post) => post.get({ plain: true }));
+
+		res.render("dashboard", {
+			dashPosts,
+			logginIn: req.session.loggedIn,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 // dashbaord: can edit user's post
+router.get("/dashboard/editpost/:id", withAuth, async (req, res) => {
+	try {
+		const editData = await Post.findByPk(req.params.id);
+
+		const editPost = editData.get({ plain: true });
+
+		res.render("editpost", {
+			editPost,
+			logginIn: req.session.loggedIn,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 // dashboard: can add new posts
+router.get("/dashboard/newpost", withAuth, async (req, res) => {
+	try {
+		res.render("newpost");
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 // login page
 router.get("/login", (req, res) => {
