e875bed06ad7b38aa82d02be94852e64c7049295
Andrew Lee <alee68@fordham.edu>
2022-07-24 02:29:25 -0400

    finished updatePost.js

--- public/js/updatePost.js
+++ public/js/updatePost.js
@@ -1,7 +1,21 @@
 const updatePost = async (event) => {
 	event.preventDefault();
 
-	const response = await fetch("/api/posts/:id", {
+	const id = document.querySelector("input[name='editPostId']").value.trim();
+	const title = document.getElementById("editPostTitle").value.trim();
+	const contents = document.getElementById("editPostContent").value.trim();
+
+	const response = await fetch(`/api/posts/${id}`, {
 		method: "PUT",
+		body: JSON.stringify({ title, contents }),
+		headers: { "Content-Type": "application/json" },
 	});
+
+	if (response.ok) {
+		document.location.replace("/dashboard");
+	} else {
+		alert("You did not update anything.");
+	}
 };
+
+document.querySelector(".editPost").addEventListener("submit", updatePost);
